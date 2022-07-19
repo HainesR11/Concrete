7f4be660652ea8692dc271ee833607e51bbe2954
Rhys Haines <rhys.haines@asda.uk>
2022-07-19 10:24:02 +0100

    main index file updated

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+//import App from './App';
 import {name as appName} from './app.json';
+import Login from './src/Unauthenticated/login';
+import React from 'react';
+
+const App = () => {
+  return <Login />;
+};
 
 AppRegistry.registerComponent(appName, () => App);
