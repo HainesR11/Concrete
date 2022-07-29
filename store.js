f59a1d74814fa8737aaa6dd2e9c2a99257ecb84a
Rhys Haines <rhys.haines@asda.uk>
2022-07-29 16:47:14 +0100

    zuzstand added

---
+++ store.js
@@ -0,0 +1,13 @@
+import create from 'zustand';
+import {devtools} from 'zustand/middleware';
+
+let rootStore = set => ({
+  userToken: false,
+  setUserToken: () => set(state => ({userToken: !state.userToken})),
+  isLoading: false,
+  setIsLoading: () => set(state => ({isLoading: !state.isLoading})),
+});
+
+rootStore = devtools(rootStore);
+
+export const useRootStore = create(rootStore);
