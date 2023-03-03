cb913d6f7c66d45790d54040de33fc530dcfc246
HainesR11 <58213900+HainesR11@users.noreply.github.com>
2023-03-03 09:56:19 +0000

    main files update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
-import RootNavigation from './src/Screens/RootNavigation';
+import App from './App';
 import { LogBox } from 'react-native';
 
 LogBox.ignoreLogs(['RCTBridge required dispatch_sync to load REAModule']);
 
-AppRegistry.registerComponent(appName, () => RootNavigation);
+AppRegistry.registerComponent(appName, () => App);
